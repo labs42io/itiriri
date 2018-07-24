ad38a10d15c5193b133827189bdf5f09d5beb3a4
Keloo <vlad-negura@mail.ru>
2018-07-24 23:42:11 +0300

    refactoring reducer/count tests

--- test/reducers/count.test.ts
+++ test/reducers/count.test.ts
@@ -1,34 +1,31 @@
 import { expect } from 'chai';
 import { query } from '../../lib/';
+import { count } from '../../lib/reducers/count';
 
 describe('reducers/count', () => {
   describe('When called on empty source', () => {
     it('Should return 0', () => {
       const arr = [];
-      const q = query(arr);
 
-      expect(q.count()).to.deep.equal(0);
+      expect(count(arr)).to.deep.equal(0);
     });
   });
 
   describe('When called on arbitrary source', () => {
     it('Should return it\'s length', () => {
       const arr = [0, 1, 2, 1];
-      const q = query(arr);
 
-      expect(q.count()).to.deep.equal(4);
+      expect(count(arr)).to.deep.equal(4);
     });
     it('Should return it\'s length', () => {
       const arr = [0];
-      const q = query(arr);
 
-      expect(q.count()).to.deep.equal(1);
+      expect(count(arr)).to.deep.equal(1);
     });
     it('Should return it\'s length', () => {
       const arr = [0, 1, 1, 1, 0, 0];
-      const q = query(arr);
 
-      expect(q.count()).to.deep.equal(6);
+      expect(count(arr)).to.deep.equal(6);
     });
   });
 });
