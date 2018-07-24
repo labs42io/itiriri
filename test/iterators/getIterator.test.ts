0c61b26ac88726f7bf83e9cf846a587209ede6d8
Keloo <vlad-negura@mail.ru>
2018-07-24 18:08:59 +0300

    iterators/getIterator test

--- test/iterators/getIterator.test.ts
+++ test/iterators/getIterator.test.ts
@@ -3,14 +3,28 @@ import { fromArray } from '../../lib/iterators/fromArray';
 import { getIterator } from '../../lib/iterators/getIterator';
 
 describe('iterators/getIterator', () => {
-  describe('when called multiple times', () => {
-    it.skip('Should return new iterator on each call', () => {
-      const source = fromArray([1, 2, 3]);
+  describe('When called on any array', () => {
+    it('Should return an iterator', () => {
+      const source = [];
+      const iterator = fromArray(source);
 
-      const iterator1 = getIterator(source);
-      const iterator2 = getIterator(source);
+      expect(iterator).to.not.be.undefined;
+    });
+    it('Should return an iterator', () => {
+      const source = [1, 2, 4, 8, 16];
+      const iterator = fromArray(source);
 
-      console.log(iterator1 === iterator2);
+      expect(iterator).to.not.be.undefined;
+    });
+  });
+
+  describe('When called multiple times', () => {
+    it('Should return new iterator on each call', () => {
+      const source = [1, 2, 3];
+      const iterator = fromArray(source);
+
+      const iterator1 = getIterator(iterator);
+      const iterator2 = getIterator(iterator);
 
       expect(iterator1).not.equals(iterator2);
     });
