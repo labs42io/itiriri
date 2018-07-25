d95f34ab828517a5eaca6d22bec52fe6dd28e861
Keloo <vlad-negura@mail.ru>
2018-07-25 11:30:13 +0300

    improving iterators/map tests

--- test/iterators/map.test.ts
+++ test/iterators/map.test.ts
@@ -18,7 +18,7 @@ describe('iterators/map', () => {
     });
   });
 
-  describe('When applying transformation', () => {
+  describe('When applying linear transformation', () => {
     it('Should return the elemens modified', () => {
       const source = [1, 2, 4, 8];
       const iterator = map(source, (elem, idx) => elem * 2 + 2);
@@ -30,6 +30,40 @@ describe('iterators/map', () => {
       const iterator = map(source, (elem, idx) => elem + 'a');
 
       expect(toArray(iterator)).to.be.deep.equal(['aa', 'ba', 'ca', 'da']);
+    });
+  });
+
+  describe('When called on empty source', () => {
+    it('Should return empty source', () => {
+      const source = [];
+      const iterator = map(source, (elem, idx) => elem + idx + 1234);
+
+      expect(toArray(iterator)).to.be.deep.equal([]);
+    });
+  });
+
+  describe('When applying index transformation', () => {
+    it('Should return the array indexes', () => {
+      const source = [10, 1, 1, 2, 3];
+      const iterator = map(source, (elem, idx) => idx);
+
+      expect(toArray(iterator)).to.be.deep.equal([0, 1, 2, 3, 4]);
+    });
+  });
+
+  describe('When applying index and value transformation', () => {
+    it('Should return the index+value array', () => {
+      const source = [2, 3, 4, 3, 2];
+      const iterator = map(source, (elem, idx) => idx + elem);
+
+      expect(toArray(iterator)).to.be.deep.equal([2, 4, 6, 6, 6]);
+    });
+
+    it('Should return the object.property + 2*index', () => {
+      const source = [{ karma: 2 }, { karma: 4 }];
+      const iterator = map(source, (elem, idx) => elem.karma + 2 * idx);
+
+      expect(toArray(iterator)).to.be.deep.equal([2, 6]);
     });
   });
 });
