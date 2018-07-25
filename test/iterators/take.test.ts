4711681b4d646aa6042417d037f31d3dae303817
Keloo <vlad-negura@mail.ru>
2018-07-25 08:32:48 +0300

    improving iterators/take tests

--- test/iterators/take.test.ts
+++ test/iterators/take.test.ts
@@ -14,19 +14,19 @@ describe('iterators/take', () => {
   });
 
   describe('When take some elements', () => {
-    it('Should return the elements', () => {
+    it('Should return 4 elements from front', () => {
       const source = fromArray([1, 2, 3, 4, 5, 6]);
       const iterator = take(source, 4);
 
       expect(toArray(iterator)).to.deep.equal([1, 2, 3, 4]);
     });
-    it('Should return the elements', () => {
+    it('Should return 2 elements from front', () => {
       const source = fromArray([1, 2, 3, 4, 5]);
       const iterator = take(source, 2);
 
       expect(toArray(iterator)).to.deep.equal([1, 2]);
     });
-    it('Should return the elements', () => {
+    it('Should return 1 element from front', () => {
       const source = fromArray([1, 2, 3]);
       const iterator = take(source, 1);
 
@@ -53,19 +53,19 @@ describe('iterators/take', () => {
   });
 
   describe('When take negative count', () => {
-    it('Should return from the tail', () => {
+    it('Should return 3 elements from tail', () => {
       const source = fromArray([1, 2, 3, 4]);
       const iterator = take(source, -3);
 
       expect(toArray(iterator)).to.deep.equal([2, 3, 4]);
     });
-    it('Should return from the tail', () => {
+    it('Should return 5 elements from tail', () => {
       const source = fromArray([1, 2, 3, 4, 5, 6]);
       const iterator = take(source, -5);
 
       expect(toArray(iterator)).to.deep.equal([2, 3, 4, 5, 6]);
     });
-    it('Should return from the tail', () => {
+    it('Should return 1 element from tail', () => {
       const source = fromArray([1, 2, 3, 4, 5]);
       const iterator = take(source, -1);
 
@@ -88,6 +88,24 @@ describe('iterators/take', () => {
       const iterator = take(source, -100);
 
       expect(toArray(iterator)).to.deep.equal([1, 2, 3, 4]);
+    });
+  });
+
+  describe('When take positive count from empty source', () => {
+    it('Should return empty source', () => {
+      const source = fromArray([]);
+      const iterator = take(source, 8);
+
+      expect(toArray(iterator)).to.deep.equal([]);
+    });
+  });
+
+  describe('When take negative count from empty source', () => {
+    it('Should return empty source', () => {
+      const source = fromArray([]);
+      const iterator = take(source, -16);
+
+      expect(toArray(iterator)).to.deep.equal([]);
     });
   });
 });
