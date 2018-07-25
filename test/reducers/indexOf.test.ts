0bb503fe991025e7e4482d02555a187f1232d9b9
Keloo <vlad-negura@mail.ru>
2018-07-25 13:03:17 +0300

    reducers/indexOf tests

---
+++ test/reducers/indexOf.test.ts
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import { indexOf } from '../../lib/reducers/indexOf';
+
+describe('reducers/indexOf', () => {
+  describe('When called on empty array', () => {
+    it('Should return -1', () => {
+      const source = [];
+
+      expect(indexOf(source, (elem, idx) => true)).to.be.equal(-1);
+    });
+  });
+
+  describe('When calle on some array', () => {
+    it('Should return the index 3', () => {
+      const source = [4, 5, 1, 20, 3];
+
+      expect(indexOf(source, (elem, idx) => elem === 20)).to.be.equal(3);
+    });
+
+    it('Should return -1 if elements does not exist', () => {
+      const source = ['a', 'b', 'z', 'aa', 'abc'];
+      expect(indexOf(source, (elem, idx) => elem === 'c')).to.be.equal(-1);
+    });
+  });
+});
