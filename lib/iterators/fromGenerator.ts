4c4ab584f01437e0e61427821576e38f125a7d65
Dumitru <dimadeveatii@gmail.com>
2018-07-27 08:50:52 +0300

    Simplifying code in fromGenerator

--- lib/iterators/fromGenerator.ts
+++ lib/iterators/fromGenerator.ts
@@ -3,12 +3,7 @@ import { getIterator } from './getIterator';
 export function fromGenerator<TElement>(generator: () => Iterable<TElement>): Iterable<TElement> {
   return {
     [Symbol.iterator]() {
-      const iterator = getIterator(generator());
-      return {
-        next() {
-          return iterator.next();
-        },
-      };
+      return getIterator(generator());
     },
   };
 }
