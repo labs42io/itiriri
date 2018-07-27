bf0ebc5c500c2d3dc7706ce2733ca2d77e66eaec
dumitru.deveatii <dimadeveatii@gmail.com>
2018-07-28 01:05:42 +0300

    improving `indexOf` tests

--- test/reducers/indexOf.test.ts
+++ test/reducers/indexOf.test.ts
@@ -6,33 +6,33 @@ describe('reducers/indexOf', () => {
     it('Should return -1', () => {
       const source = [];
 
-      expect(indexOf(source, (elem, idx) => true)).to.be.equal(-1);
+      expect(indexOf(source, () => true)).to.be.equal(-1);
     });
   });
 
-  describe('When calle on some array', () => {
+  describe('When called on some array', () => {
     it('Should return the index 3', () => {
       const source = [4, 5, 1, 20, 3];
 
-      expect(indexOf(source, (elem, idx) => elem === 20)).to.be.equal(3);
+      expect(indexOf(source, elem => elem === 20)).to.be.equal(3);
     });
 
     it('Should return the index 0', () => {
       const source = [1, 2, 1, 3, 4];
 
-      expect(indexOf(source, (elem, idx) => elem === 1)).to.be.equal(0);
+      expect(indexOf(source, elem => elem === 1)).to.be.equal(0);
     });
 
     it('Should return -1 if elements does not exist', () => {
       const source = ['a', 'b', 'z', 'aa', 'abc'];
 
-      expect(indexOf(source, (elem, idx) => elem === 'c')).to.be.equal(-1);
+      expect(indexOf(source, elem => elem === 'c')).to.be.equal(-1);
     });
 
     it('Should return the index of last element', () => {
       const source = [5, 7, 1, 3];
 
-      expect(indexOf(source, (elem, idx) => elem === 3)).to.be.equal(3);
+      expect(indexOf(source, elem => elem === 3)).to.be.equal(3);
     });
   });
 
