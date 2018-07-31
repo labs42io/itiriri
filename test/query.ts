9ae5652f380c097ad299fdef7b7eb8511e499ac5
Keloo <vlad-negura@mail.ru>
2018-07-31 23:29:39 +0300

    query.exclude tests

--- test/query.ts
+++ test/query.ts
@@ -974,4 +974,36 @@ describe('Query', () => {
       ]);
     });
   });
+
+  describe('When calling exclude', () => {
+    it('Should return array of 1 elements', () => {
+      const source1 = new SpyIterable([0, 4, 4, 0]);
+      const source2 = new SpyIterable([0, 5]);
+      const q = query(source1).exclude(source2, x => x);
+
+      expect(source1.wasIterated).to.be.false;
+      expect(source2.wasIterated).to.be.false;
+      expect(q.toArray()).to.be.deep.equal([4]);
+    });
+
+    it('Should return array of 3 elements', () => {
+      const source1 = new SpyIterable([
+        { val: 1, tag: 'a' },
+        { val: 2, tag: 'b' },
+        { val: 3, tag: 'a' },
+        { val: 4, tag: 'a' },
+        { val: 5, tag: 'b' },
+        { val: 6, tag: 'c' },
+      ]);
+      const source2 = new SpyIterable([{ val: 10, tag: 'a' }]);
+      const q = query(source1).exclude(source2, x => x.tag);
+
+      expect(source1.wasIterated).to.be.false;
+      expect(source2.wasIterated).to.be.false;
+      expect(q.toArray()).to.be.deep.equal([
+        { val: 2, tag: 'b' },
+        { val: 6, tag: 'c' },
+      ]);
+    });
+  });
 });
