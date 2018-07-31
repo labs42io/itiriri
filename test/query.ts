5e4206d69736df4724d0fb55512ddbf32224c12a
Keloo <vlad-negura@mail.ru>
2018-07-31 17:14:15 +0300

    query.every tests

--- test/query.ts
+++ test/query.ts
@@ -657,4 +657,39 @@ describe('Query', () => {
       expect(q.reduceRight((x, e, idx) => x + e.tag, '')).to.be.equal('cba');
     });
   });
+
+  describe('When calling every', () => {
+    it('Should return true on array', () => {
+      const source = new SpyIterable([0, 4, 4, 30, 10, 10]);
+      const q = query(source);
+
+      expect(source.wasIterated).to.be.false;
+      expect(q.every(x => x >= 0)).to.be.true;
+    });
+    it('Should return true on empty source', () => {
+      const source = new SpyIterable([]);
+      const q = query(source);
+
+      expect(source.wasIterated).to.be.false;
+      expect(q.every(x => x * 20 === 0)).to.be.true;
+    });
+    it('Should return false on array of objects', () => {
+      const source = new SpyIterable([
+        { val: 10, tag: 'a' }, { val: 20, tag: 'b' }, { val: -10, tag: 'c' },
+      ]);
+      const q = query(source);
+
+      expect(source.wasIterated).to.be.false;
+      expect(q.every(x => x.val <= 10)).to.be.false;
+    });
+    it('Should return true on array of objects', () => {
+      const source = new SpyIterable([
+        { val: 10, tag: 'a' }, { val: 20, tag: 'b' }, { val: -10, tag: 'c' },
+      ]);
+      const q = query(source);
+
+      expect(source.wasIterated).to.be.false;
+      expect(q.every((elem, idx) => idx < 10)).to.be.true;
+    });
+  });
 });
