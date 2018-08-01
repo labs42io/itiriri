866984aa4bbd74236c3df227c8e8f9d80cc77231
Keloo <vlad-negura@mail.ru>
2018-08-01 10:16:40 +0300

    query.leftJoin tests, query.rightJoin tests TDD

--- test/query.ts
+++ test/query.ts
@@ -1203,4 +1203,40 @@ describe('Query', () => {
       expect(q.toArray()).to.deep.equal([3, 113, 3333]);
     });
   });
+
+  describe('When calling leftJoin', () => {
+    it('Should return array of 4 elements', () => {
+      const source1 = new SpyIterable([0, 4, 5, 1]);
+      const source2 = new SpyIterable([-1, 4, 5, -1]);
+      const q = query(source1).leftJoin(source2, x => x, x => x, (e1, e2) => ({ e1, e2 }));
+
+      expect(source1.wasIterated).to.be.false;
+      expect(source2.wasIterated).to.be.false;
+
+      expect(q.toArray()).to.deep.equal([
+        { e1: 0, e2: undefined },
+        { e1: 4, e2: 4 },
+        { e1: 5, e2: 5 },
+        { e1: 1, e2: undefined },
+      ]);
+    });
+  });
+
+  describe.skip('When calling rightJoin', () => {
+    it('Should return array of 1 elements', () => {
+      const source1 = new SpyIterable([0, 4, 5, 1]);
+      const source2 = new SpyIterable([-1, 4, 5, -2]);
+      const q = query(source1).rightJoin(source2, x => x, x => x, (e1, e2) => ({ e1, e2 }));
+
+      expect(source1.wasIterated).to.be.false;
+      expect(source2.wasIterated).to.be.false;
+
+      expect(q.toArray()).to.deep.equal([
+        { e1: undefined, e2: -1 },
+        { e1: 4, e2: 4 },
+        { e1: 5, e2: 5 },
+        { e1: undefined, e2: -2 },
+      ]);
+    });
+  });
 });
