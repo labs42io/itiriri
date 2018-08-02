0b961783f59c58be66433a56ddee049aeca3c0d8
Keloo <vlad-negura@mail.ru>
2018-08-02 11:20:50 +0300

    improving isIterable, + tests

--- lib/utils/isIterable.ts
+++ lib/utils/isIterable.ts
@@ -1,3 +1,3 @@
 export function isIterable<T>(item: any): item is Iterable<T> {
-  return (<Iterable<T>>item)[Symbol.iterator] !== undefined;
+  return typeof (<Iterable<T>>item)[Symbol.iterator] === 'function';
 }
---
+++ test/utils/isIterable.test.ts
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import { isIterable } from '../../lib/utils/isIterable';
+import { query } from '../../lib/Query';
+
+describe('utils/isIterable', () => {
+  describe('When called on array', () => {
+    it('Should return true on empty array', () => {
+      expect(isIterable([])).to.be.true;
+    });
+    it('Should return true on non-empty array', () => {
+      expect(isIterable([1, 'a'])).to.be.true;
+    });
+  });
+  describe('When called on string', () => {
+    it('Should return true on empty string', () => {
+      expect(isIterable('')).to.be.true;
+    });
+    it('Should return true on non-empty string', () => {
+      expect(isIterable('asdf')).to.be.true;
+    });
+  });
+  describe('When called on Query', () => {
+    it('Should return true', () => {
+      expect(isIterable(query([]))).to.be.true;
+    });
+  });
+  describe('When called on Number', () => {
+    it('Should return false', () => {
+      expect(isIterable(4)).to.be.false;
+    });
+  });
+  describe('When called on Object', () => {
+    it('Should return false', () => {
+      expect(isIterable({ a: 1, b: 'a' })).to.be.false;
+    });
+  });
+});
